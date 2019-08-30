14f0c2f19396edac2ddb8f56a3c557e9bf8fdc9d
Thijs Vogels <thijs.vogels@epfl.ch>
2019-08-30 10:28:38 +0200

    Do moves before fade in and out

--- src/slideplayer.js
+++ src/slideplayer.js
@@ -100,6 +100,86 @@ class Stage {
         this.index = new MoveElementIndex(this.dom);
         this.nextIndex = new MoveElementIndex(nextStep.dom);
 
+        for (let node of this.dom.querySelectorAll("[move]")) {
+            const id = node.getAttribute("id");
+            const nodeInNextStage = this.nextIndex.nodeByIdentifier(id);
+            if (nodeInNextStage == null) continue; // no match ... too bad
+
+            const attributes = new Set([...node.getAttributeNames(), ...nodeInNextStage.getAttributeNames()]);
+            for (let attribute of attributes) {
+                let defaultValue = { opacity: 1, transform: "", style: "" }[attribute];
+                if (defaultValue == null) {
+                    defaultValue = 0.0;
+                }
+                const currentValue = node.getAttribute(attribute) || defaultValue;
+                const nextValue = nodeInNextStage.getAttribute(attribute) || defaultValue;
+                if (currentValue !== nextValue) {
+                    if (["d"].includes(attribute)) {
+                        // We are using snap.svg for morphing between SVG paths
+                        let eq = snap(node).equal(attribute, nextValue);
+                        this._addTransition(
+                            this._getTransitionDuration(node, "move"),
+                            this._getTransitionAlignment(node, "move"),
+                            "easeInOutQuad",
+                            dt => {
+                                node.setAttribute(attribute, eq.f(linearMix(eq.from, eq.to, dt)));
+                            }
+                        );
+                    } else if (["fill", "stroke"].includes(attribute)) {
+                        const c1 = snap.color(currentValue);
+                        const c2 = snap.color(nextValue);
+                        const from = [c1.r, c1.g, c1.b, c1.opacity];
+                        const to = [c2.r, c2.g, c2.b, c2.opacity];
+                        this._addTransition(
+                            this._getTransitionDuration(node, "move"),
+                            this._getTransitionAlignment(node, "move"),
+                            "easeInOutQuad",
+                            dt => {
+                                node.setAttribute(attribute, snap.rgb(...linearMix(from, to, dt)));
+                            }
+                        );
+                    } else if (
+                        ["x", "y", "opacity", "rx", "height", "width", "cx", "cy", "r", "fill-opacity"].includes(
+                            attribute
+                        )
+                    ) {
+                        this._addTransition(
+                            this._getTransitionDuration(node, "move"),
+                            this._getTransitionAlignment(node, "move"),
+                            "easeInOutQuad",
+                            dt => {
+                                node.setAttribute(
+                                    attribute,
+                                    linearMix(parseFloat(currentValue), parseFloat(nextValue), dt)
+                                );
+                            }
+                        );
+                    } else if (attribute === "transform") {
+                        const a = new CSSTransform(currentValue);
+                        const b = new CSSTransform(nextValue);
+                        this._addTransition(
+                            this._getTransitionDuration(node, "move"),
+                            this._getTransitionAlignment(node, "move"),
+                            "easeInOutQuad",
+                            dt => {
+                                node.setAttribute(attribute, a.mixString(b, dt));
+                            }
+                        );
+                    } else if (
+                        ["move", "fade-in", "fade-out", "appear-along", "draw-line", "class", "stage"].includes(
+                            attribute
+                        )
+                    ) {
+                        // Nothing to do
+                    } else {
+                        console.error(
+                            `Don't know how to handle transitions for attribute '${attribute}' (from ${currentValue} to ${nextValue}), node ${node.id}`
+                        );
+                    }
+                }
+            }
+        }
+
         // Entry effect: fade-in
         for (let node of nextStep.dom.querySelectorAll("[fade-in]")) {
             const ghostNode = this._insertGhostNode(node);
@@ -169,83 +249,6 @@ class Stage {
                     node.style.opacity = linearMix(startOpacity, 0.0, dt);
                 }
             );
-        }
-
-        for (let node of this.dom.querySelectorAll("[move]")) {
-            const id = node.getAttribute("id");
-            const nodeInNextStage = this.nextIndex.nodeByIdentifier(id);
-            if (nodeInNextStage == null) continue; // no match ... too bad
-            const attributes = new Set([...node.getAttributeNames(), ...nodeInNextStage.getAttributeNames()]);
-            for (let attribute of attributes) {
-                let defaultValue = { opacity: 1, transform: "" }[attribute];
-                if (defaultValue == null) {
-                    defaultValue = 0.0;
-                }
-                const currentValue = node.getAttribute(attribute) || defaultValue;
-                const nextValue = nodeInNextStage.getAttribute(attribute) || defaultValue;
-                if (currentValue !== nextValue) {
-                    if (["d"].includes(attribute)) {
-                        // We are using snap.svg for morphing between SVG paths
-                        let eq = snap(node).equal(attribute, nextValue);
-                        this._addTransition(
-                            this._getTransitionDuration(node, "move"),
-                            this._getTransitionAlignment(node, "move"),
-                            "easeInOutQuad",
-                            dt => {
-                                node.setAttribute(attribute, eq.f(linearMix(eq.from, eq.to, dt)));
-                            }
-                        );
-                    } else if (["fill", "stroke"].includes(attribute)) {
-                        const c1 = snap.color(currentValue);
-                        const c2 = snap.color(nextValue);
-                        const from = [c1.r, c1.g, c1.b, c1.opacity];
-                        const to = [c2.r, c2.g, c2.b, c2.opacity];
-                        this._addTransition(
-                            this._getTransitionDuration(node, "move"),
-                            this._getTransitionAlignment(node, "move"),
-                            "easeInOutQuad",
-                            dt => {
-                                node.setAttribute(attribute, snap.rgb(...linearMix(from, to, dt)));
-                            }
-                        );
-                    } else if (
-                        ["x", "y", "opacity", "rx", "height", "width", "cx", "cy", "r", "fill-opacity"].includes(
-                            attribute
-                        )
-                    ) {
-                        this._addTransition(
-                            this._getTransitionDuration(node, "move"),
-                            this._getTransitionAlignment(node, "move"),
-                            "easeInOutQuad",
-                            dt => {
-                                node.setAttribute(
-                                    attribute,
-                                    linearMix(parseFloat(currentValue), parseFloat(nextValue), dt)
-                                );
-                            }
-                        );
-                    } else if (attribute === "transform") {
-                        const a = new CSSTransform(currentValue);
-                        const b = new CSSTransform(nextValue);
-                        this._addTransition(
-                            this._getTransitionDuration(node, "move"),
-                            this._getTransitionAlignment(node, "move"),
-                            "easeInOutQuad",
-                            dt => {
-                                node.setAttribute(attribute, a.mixString(b, dt));
-                            }
-                        );
-                    } else if (
-                        ["move", "fade-in", "fade-out", "appear-along", "draw-line", "class", "stage"].includes(
-                            attribute
-                        )
-                    ) {
-                        // Nothing to do
-                    } else {
-                        console.error(`Don't know how to handle transitions for attribute '${attribute}'`);
-                    }
-                }
-            }
         }
     }
 
