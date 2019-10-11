1461f31dbb1c0581f9e44db964e5d724c9c1ebc1
Thijs Vogels <thijs.vogels@epfl.ch>
2019-10-11 14:25:23 +0200

    added per-slide time recording to be able to play back later

--- src/recording.js
+++ src/recording.js
@@ -1,5 +1,6 @@
 import html2canvas from "html2canvas";
 import "regenerator-runtime/runtime";
+import moment from "moment";
 
 function downloadDataUrl(dataUrl, filename) {
     var a = document.createElement("a");
@@ -8,17 +9,70 @@ function downloadDataUrl(dataUrl, filename) {
     a.click();
 }
 
-export async function record({ fps = 60, scale = 1.5 }) {
+export function recordDurations(controller) {
+    const durations = [];
+
+    let current = {
+        pos: controller.currentPosition,
+        start: moment.now()
+    };
+
+    controller.addRenderListener(dt => {
+        if (current != null && dt !== current.pos) {
+            durations.push({
+                step: current.pos,
+                start: current.start,
+                duration: moment.now() - current.start
+            });
+            current = null;
+        }
+        if (dt % 1 === 0) {
+            current = {
+                pos: dt,
+                start: moment.now()
+            };
+        }
+    });
+
+    return () => {
+        if (current == null) {
+            return durations;
+        } else {
+            return [
+                ...durations,
+                {
+                    step: current.pos,
+                    start: current.start,
+                    duration: moment.now() - current.start
+                }
+            ];
+        }
+    };
+}
+
+export async function record(durationSpec, fps = 24, scale = 1.5) {
     // /usr/local/bin/ffmpeg -r 60 -i frame%05d.png -c:v libx264 -vf "fps=60,format=yuv420p" -an out.mp4
     //                          input framerate                           output framerate
+
+    const defaultFrameDuration = 0.5; // seconds
 
     const screen = document.getElementById("main-screen");
     const deck = window.slides.deck;
     const controller = window.slides;
 
-    screen.style.transform = `scale(${scale})`;
-
     let frame = 0;
+
+    if (durationSpec == null) {
+        durationSpec = [];
+        for (let i = 0; i < deck.numSteps(); ++i) {
+            durationSpec.push({
+                step: i,
+                duration: defaultFrameDuration * 1000
+            });
+        }
+    }
+
+    screen.style.transform = `scale(${scale})`;
 
     function storeFrame() {
         return html2canvas(screen).then(canvas => {
@@ -27,17 +81,17 @@ export async function record({ fps = 60, scale = 1.5 }) {
         });
     }
 
-    for (let i = 0; i < deck.numSteps(); ++i) {
-        controller.setPosition(i);
+    for (let { step, duration } of durationSpec) {
+        controller.setPosition(step);
         // Store half a second of the frame
-        for (let f = 0; f < fps / 2; ++f) {
+        for (let f = 0; f < (fps * duration) / 1000; ++f) {
             await storeFrame();
         }
 
         // Do the transition
-        const duration = controller._durationBetweenPoints(i, i + 1);
-        for (let f = 0; f < fps * duration; ++f) {
-            controller.setPosition(i + f / fps / duration);
+        const transitionDuration = controller._durationBetweenPoints(step, step + 1);
+        for (let f = 0; f < fps * transitionDuration; ++f) {
+            controller.setPosition(step + f / fps / transitionDuration);
             await storeFrame();
         }
     }
