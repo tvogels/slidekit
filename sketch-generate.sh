dcbe04ac15b3d11fd5c488ad93bd29ec83a2e575
Jean-Baptiste Cordonnier <jbcdnr@gmail.com>
2020-04-03 12:00:58 +0200

    Add starter pack submodule and update build script

--- sketch-generate.sh
+++ sketch-generate.sh
@@ -7,10 +7,13 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# generate data_dir path_to_preprocess_slides.py
 function generate {
     rm -rf "$1/*.svg"
     sketchtool export artboards --formats=svg --output="$1" "$1/slides.sketch"
-    ./preprocess_slides.py --output "$1/slides.json" --media-out-dir "$1/dist" "$1"
+    $2 --output "$1/slides.json" --media-out-dir "$1/dist" "$1"
 }
 
 export -f generate
@@ -18,4 +21,4 @@ export -f generate
 directory="$1"
 svg_file="${directory}/slides.sketch"
 echo "Watching update of file ${svg_file}..."
-fswatch -o "$svg_file" | xargs -n1 -I{} bash -c "generate '$directory'"
+fswatch -o "$svg_file" | xargs -n1 -I{} bash -c "generate '$directory' '$SCRIPT_DIR/preprocess_slides.py'"
