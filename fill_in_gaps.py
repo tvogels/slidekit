9dc931275505a3836fed4a61929ddffa7d4cc522
Thijs Vogels <thijs.vogels@epfl.ch>
2021-10-19 13:46:57 +0200

    added script to complete frame sequences with missing entries

---
+++ fill_in_gaps.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3 
+
+import glob
+import shutil
+import re
+
+frame_dict = {}
+
+max_frame = 0
+
+for file in glob.glob("frame*.png"):
+    out = re.match(r"""frame(\d+).png""", file)
+    frameno = int(out[1])
+    frame_dict[frameno] = file
+    max_frame = max(max_frame, frameno)
+
+prev_match = 0
+for f in range(max_frame):
+    if f in frame_dict:
+        prev_match = f
+    else:
+        outfile = f"frame{f:06d}.png"
+        print("Writing", outfile)
+        shutil.copyfile(frame_dict[prev_match], outfile)
