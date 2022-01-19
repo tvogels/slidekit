431eb6e08c6ca0fb38d1ee22ba4d59263ed247f6
Thijs Vogels <thijs.vogels@epfl.ch>
2022-01-19 22:26:04 +0100

    changed from demo-data to usage example

--- example/index.js
+++ example/index.js
@@ -1,10 +1,9 @@
-import { Controller, SlideDeck, PresenterNotes } from "../src";
-import { youtubePlugin } from "../src/plugins";
+import { Controller, SlideDeck, PresenterNotes } from "svgslides";
+import { youtubePlugin } from "svgslides";
 import moment from "moment";
 
 import slides from "./slides.json"; // generated with preprocess_slides.py
-import notes from "./notes.md";
-import "./custom-style.css";
+import notes from "./presenter-notes.md";
 
 window.slides = new Controller(
     new SlideDeck(slides, [youtubePlugin]),
