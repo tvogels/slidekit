c8f7a9cc4db99c85aa87c13c122710ce63f53bce
Thijs Vogels <thijs.vogels@epfl.ch>
2022-01-19 22:26:17 +0100

    fixed export of plugins

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ export { default as Controller } from "./controller";
 export { default as SlideDeck } from "./slidedeck";
 export { default as PresenterNotes } from "./presenternotes";
 
-export { youtubePlugin, scalePlugin } from "./plugins"
+export { youtubePlugin as youtubePlugin, scalePlugin as scalePlugin } from "./plugins"
 
 import "./slides.css";
