931fe45e2e8319e5884b25d71ad4297491ddea64
Thijs Vogels <thijs.vogels@epfl.ch>
2022-01-19 18:44:03 +0100

    exported plugins for npm

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ export { default as Controller } from "./controller";
 export { default as SlideDeck } from "./slidedeck";
 export { default as PresenterNotes } from "./presenternotes";
 
+export { youtubePlugin, scalePlugin } from "./plugins"
+
 import "./slides.css";
