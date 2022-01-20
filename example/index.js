44988d80c10f47185eea41823ba45088ba96e5e1
Thijs Vogels <thijs.vogels@epfl.ch>
2022-01-20 19:10:10 +0100

    working on example

--- example/index.js
+++ example/index.js
@@ -6,7 +6,7 @@ import slides from "./slides.json"; // generated with preprocess_slides.py
 import notes from "./presenter-notes.md";
 
 window.slides = new Controller(
-    new SlideDeck(slides, [youtubePlugin]),
+    new SlideDeck(slides, []),
     document.getElementById("main-screen"),
     moment.duration(30, "minutes"),
     new PresenterNotes(notes)
