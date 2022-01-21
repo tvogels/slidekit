8b32fcb1233a38c15eff21e99ffdbf6ba99138ea
Thijs Vogels <thijs.vogels@epfl.ch>
2022-01-21 20:04:11 +0100

    updated example

--- example/index.js
+++ example/index.js
@@ -1,12 +1,13 @@
-import { Controller, SlideDeck, PresenterNotes } from "svgslides";
-import { youtubePlugin } from "svgslides";
+import { Controller, SlideDeck, PresenterNotes, plugins } from "svgslides";
 import moment from "moment";
 
-import slides from "./slides.json"; // generated with preprocess_slides.py
+import slides from "./*.svg";
 import notes from "./presenter-notes.md";
 
+const slideList = Object.entries(slides).map(([key, value]) => ({ id: key, content: value}));
+
 window.slides = new Controller(
-    new SlideDeck(slides, []),
+    new SlideDeck(slideList, [plugins.youtubePlugin, plugins.hyperlinkPlugin]),
     document.getElementById("main-screen"),
     moment.duration(30, "minutes"),
     new PresenterNotes(notes)
