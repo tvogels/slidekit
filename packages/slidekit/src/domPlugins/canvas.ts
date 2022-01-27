1b93ef6349b5a75b255849be15ccb818d6a79cd1
Thijs Vogels <thijs.vogels@epfl.ch>
2022-01-27 10:02:25 +0100

    Revert "use bounding box for foreign objects instead of relying on rect"
    This reverts commit 2c530f3deffdb244c18104c510ac8c5f21497e14.

--- packages/slidekit/src/domPlugins/canvas.ts
+++ packages/slidekit/src/domPlugins/canvas.ts
@@ -2,13 +2,14 @@ export default function (dom: HTMLElement) {
     for (let node of [...dom.querySelectorAll("[canvas]")]) {
         const foreignObject = document.createElementNS("http://www.w3.org/2000/svg", "foreignObject");
 
-        const { width, height, x, y } = node.getBoundingClientRect();
-        foreignObject.setAttribute("x", `${x}`);
-        foreignObject.setAttribute("y", `${y}`);
-        foreignObject.setAttribute("height", `${height}`);
-        foreignObject.setAttribute("width", `${width}`);
+        for (let { name, value } of node.attributes) {
+            if (["stroke", "fill", "stroke-width", "fill-rule", "canvas"].indexOf(name) >= 0) continue;
+            foreignObject.setAttribute(name, value);
+        }
 
-        foreignObject.innerHTML = `<canvas width="${width}" height="${height}" />`;
+        foreignObject.innerHTML = `<canvas width="${node.getAttribute("width")}" height="${node.getAttribute(
+            "height"
+        )}" />`;
         const canvas = foreignObject.querySelector("canvas")
         canvas.id = foreignObject.id + "-canvas";
 
