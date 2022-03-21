ff7aa2a8062af35eff425bb0b9987741a3fc128a
Thijs Vogels <thijs.vogels@epfl.ch>
2022-03-21 14:33:35 +0100

    fix draw line and sketch for lines

--- packages/slidekit/src/transitions/drawLine.ts
+++ packages/slidekit/src/transitions/drawLine.ts
@@ -4,15 +4,15 @@ import { parseTransitionDuration, parseTransitionAlignment } from "./utils";
 export const attribute = "draw-line";
 
 export function create(node: HTMLElement, ghostNode: HTMLElement) {
-    const length = (node as any as SVGPathElement).getTotalLength();
-    ghostNode.style.strokeDasharray = length.toString();
-    ghostNode.style.strokeDashoffset = length.toString();
+    ghostNode.setAttribute("pathLength", "1");
+    ghostNode.style.strokeDasharray = "1";
+    ghostNode.style.strokeDashoffset = "1";
     return [{
         duration: parseTransitionDuration(node, "draw-line", 0.5, "enter"),
         alignment: parseTransitionAlignment(node, "draw-line", 1.0, "enter"),
         mode: "easeInOutQuad",
         callback: dt => {
-            ghostNode.style.strokeDashoffset = linearMix(length, 0.0, dt);
+            ghostNode.style.strokeDashoffset = linearMix(1.0, 0.0, dt);
         }
     }]
 };
--- packages/slidekit/src/transitions/sketch.ts
+++ packages/slidekit/src/transitions/sketch.ts
@@ -4,9 +4,10 @@ import { parseTransitionDuration, parseTransitionAlignment } from "./utils";
 export const attribute = "sketch";
 
 export function create(node: HTMLElement, ghostNode: HTMLElement) {
-    const length = (node as any as SVGPathElement).getTotalLength();
-    ghostNode.style.strokeDasharray = length.toString();
-    ghostNode.style.strokeDashoffset = length.toString();
+    // const length = (node as any as SVGPathElement).getTotalLength();
+    ghostNode.setAttribute("pathLength", "1");
+    ghostNode.style.strokeDasharray = "1";
+    ghostNode.style.strokeDashoffset = "1";
     (node as HTMLElement).style.opacity = "0";
     return [{
         duration: parseTransitionDuration(node, "sketch", 0.5, "enter"),
@@ -14,9 +15,9 @@ export function create(node: HTMLElement, ghostNode: HTMLElement) {
         mode: "easeInOutQuad",
         callback: dt => {
             if (dt < 0.5) {
-                ghostNode.style.strokeDashoffset = linearMix(length, 0.0, dt * 2);
+                ghostNode.style.strokeDashoffset = linearMix(1., 0.0, dt * 2);
             } else {
-                ghostNode.style.strokeDashoffset = linearMix(2 * length, length, (dt - 0.5) * 2);
+                ghostNode.style.strokeDashoffset = linearMix(2., 1.0, (dt - 0.5) * 2);
             }
         }
     }]
